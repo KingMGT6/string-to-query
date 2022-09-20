d781c004fdec0e4665623873bcfa060804f6565d
torresmarlon0627@yahoo.com <torresmarlon0627@yahoo.com>
2022-09-20 21:56:54 +0800

    First commit

---
+++ STQ/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace STQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.picbconvert = new System.Windows.Forms.PictureBox();
+            this.lblclear = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picbconvert)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(318, 434);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(421, 6);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(318, 434);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // picbconvert
+            // 
+            this.picbconvert.BackColor = System.Drawing.Color.Transparent;
+            this.picbconvert.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picbconvert.Image = global::STQ.Properties.Resources.giphy;
+            this.picbconvert.Location = new System.Drawing.Point(337, 172);
+            this.picbconvert.Name = "picbconvert";
+            this.picbconvert.Size = new System.Drawing.Size(78, 70);
+            this.picbconvert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbconvert.TabIndex = 4;
+            this.picbconvert.TabStop = false;
+            this.picbconvert.Click += new System.EventHandler(this.picbconvert_Click);
+            this.picbconvert.MouseLeave += new System.EventHandler(this.picbconvert_MouseLeave);
+            this.picbconvert.MouseHover += new System.EventHandler(this.picbconvert_MouseHover);
+            // 
+            // lblclear
+            // 
+            this.lblclear.BackColor = System.Drawing.Color.Transparent;
+            this.lblclear.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblclear.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblclear.Location = new System.Drawing.Point(337, 254);
+            this.lblclear.Name = "lblclear";
+            this.lblclear.Size = new System.Drawing.Size(78, 49);
+            this.lblclear.TabIndex = 5;
+            this.lblclear.Text = "CLEAR TEXT";
+            this.lblclear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblclear.Click += new System.EventHandler(this.lblclear_Click);
+            this.lblclear.MouseLeave += new System.EventHandler(this.lblclear_MouseLeave);
+            this.lblclear.MouseHover += new System.EventHandler(this.lblclear_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(752, 457);
+            this.Controls.Add(this.lblclear);
+            this.Controls.Add(this.picbconvert);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String to Query";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picbconvert)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.PictureBox picbconvert;
+        private System.Windows.Forms.Label lblclear;
+    }
+}
+
---
+++ STQ/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picbconvert_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text != string.Empty)
+            {
+                string[] testarr = richTextBox1.Text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                richTextBox2.Text = "('" + string.Join("','", testarr) + "')";
+            }
+        }
+
+        private void picbconvert_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip tt = new ToolTip();
+            tt.SetToolTip(this.picbconvert, "Convert to query");
+            picbconvert.BackColor = Color.Yellow;
+        }
+
+        private void picbconvert_MouseLeave(object sender, EventArgs e)
+        {
+            picbconvert.BackColor = Color.Transparent;
+        }
+
+        private void lblclear_MouseHover(object sender, EventArgs e)
+        {
+            lblclear.BackColor = Color.Yellow;
+        }
+
+        private void lblclear_MouseLeave(object sender, EventArgs e)
+        {
+            lblclear.BackColor = Color.Transparent;
+        }
+
+        private void lblclear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox2.ReadOnly = true;
+        }
+    }
+}
